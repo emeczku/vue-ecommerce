4528582539a16a6393231a879691a679827c1195
Przemysław Majewski <dzius89@gmail.com>
2023-02-22 22:10:51 +0100

    project init

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+    'plugin:vitest-globals/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    "prettier/prettier": [0, { "singleQuote": false }]
+  },
+  env: {
+    'vitest-globals/env': true
+  }
+}
